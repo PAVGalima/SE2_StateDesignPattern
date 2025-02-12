787e138ee70ca17e4dc22f9d1844fdc38b20e5f0
Galima Patrick Ace <patrickace.galima@neu.edu.ph>
2025-02-12 06:18:24 +0000

    done

--- AccountTest.java
+++ AccountTest.java
@@ -1,13 +1,14 @@
 public class AccountTest {
     public static void main(String[] args) {
 
+        System.out.println();
         Account myAccount = new Account("1234", 10000.0); // Active State
         myAccount.activate(); 
-
+        System.out.println();
         myAccount.suspend(); 
 
         myAccount.activate(); 
-
+        System.out.println();
         // Deposit to the account
         myAccount.deposit(1000.0); 
 
@@ -16,12 +17,13 @@ public class AccountTest {
 
         // Close the account
         myAccount.close(); 
-
+        System.out.println();
         // Try to activate a closed account
         myAccount.activate(); 
 
         myAccount.suspend(); 
         myAccount.withdraw(500.0); 
         myAccount.deposit(1000.0); 
+        System.out.println();
     }
 }
